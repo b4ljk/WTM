6e327ab281c69b590340d895eb644ee6447b6578
baljinnyam <baljinnyam.dayan@gmail.com>
2022-10-12 17:02:12 +0800

    screen 3

---
+++ components/page2.js
@@ -0,0 +1,30 @@
+import { Box, Container, Text, Image } from "@chakra-ui/react";
+export default function Page2() {
+	return (
+		<Container py="16" maxW={"container.md"}>
+			<Text textAlign={"center"}>
+				Google компани 2012 оноос эхлэн технологийн салбар дахь эмэгтэйчүүдийг дэмжих, урам зориг өгөх, мөн энэ
+				салбар дахь хүйсийн тэнцвэрт байдлыг нэмэгдүүлэх зорилгоор Women Techmakers хөтөлбөрийг эхлүүлсэн нь
+				өнөөг хүртэл дэлхийн
+			</Text>
+
+			<Box
+				py="12"
+				display={"flex"}
+				flexDir={{ base: "column", md: "row" }}
+				justifyContent="center"
+				alignItems={"center"}
+			>
+				<Image height={"24"} src="/2_worldwide.png" />
+				<Text pt="4" ml="2" fontSize={"5xl"} fontWeight="black">
+					190+
+				</Text>
+				<Image height={"24"} src="/2_women.png" />
+				<Text pt="4" ml="2" fontSize={"5xl"} fontWeight="black">
+					200k+
+				</Text>
+			</Box>
+			<Text textAlign={"center"}>мянга гаруй эмэгтэйчүүдэд амжилттай хүрсээр байна. </Text>
+		</Container>
+	);
+}
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,32 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Container, Text, Image, Flex } from "@chakra-ui/react";
 import Head from "next/head";
-import Image from "next/image";
 import Footer from "../components/footer";
 import Header from "../components/header";
+import Page2 from "../components/page2";
 import styles from "../styles/Home.module.css";
 export default function Home() {
 	return (
 		<div>
 			<Header />
 			<Box height={"90vh"} bg="facebook.200"></Box>
+			<Box minHeight={"40vh"}>
+				<Page2 />
+			</Box>
+			<Box minH={"40vh"}>
+				<Container py="16" maxW={"container.md"}>
+					<Flex>
+						<Text fontWeight={"bold"} textAlign={"center"}>
+							Нээлттэй, олныг хамарсан хүрээлэл бий болгох
+						</Text>
+						<Text fontWeight={"bold"} textAlign={"center"}>
+							Бодит, хэрэгцээт контент мэдээллээр хангах
+						</Text>
+						<Text fontWeight={"bold"} textAlign={"center"}>
+							Үйл ажиллагаа, арга хэмжээ зохион байгуулах
+						</Text>
+					</Flex>
+				</Container>
+			</Box>
 			<Footer />
 		</div>
 	);
